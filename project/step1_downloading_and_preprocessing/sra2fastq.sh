9af79e55925957f9c1a11b071f25db0ad16461a3
Luchao Qi <lc.qi@outlook.com>
2019-05-08 14:27:42 -0700

    lc

--- project/step1_downloading_and_preprocessing/sra2fastq.sh
+++ project/step1_downloading_and_preprocessing/sra2fastq.sh
@@ -2,3 +2,6 @@
 # just check fastq-dump --h
 fastq-dump --gzip --split-3 -O ./ your sra path
 
+# e.g.
+ls /public/project/RNA/airway/sra/* | while read id; do ( nohup fastq-dump --gzip --split-3 -O ./ $id &); done
+
