b85e91b1a3bb00542263cd6d2ec4a70881e07dd5
Luchao Qi <lc.qi@outlook.com>
2019-05-10 12:00:31 -0700

    lc

---
+++ project_Homo_sapiens/3_alignment/alignment.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# hisat2,subjunc,star,bwa,bowtie2 can be used to do alignment
+
+# note need to create index at first
+# subsample: cat for fq, zcat for gz
+# if no need to keep .gz as file extension use basename $id ".gz"
+ls /home/leon/rna-seq/project_Homo_sapiens/2_quality_control/trim_galore_output/*.fq| while read id; do ( cat $id|head -1000 > $(basename $id ));done
+
+# bowtie2
+
+bowtie2-build chr22.fa chr22
+bowtie2 -x chr22 -1 sample/pair.1.fq -2 sample/pair.2.fq -S sample.sam
+
+
+# hisat2
+hisat2-build ref.fa ref
+hisat2 -p 10 -x ref -1 $fq1 -2 $fq2 -S temp.sam
+
+# subjunc
+# build index at first!
+subjunc -T 5 -i 
