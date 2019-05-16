56dfa09a711b8574e430af6cadc81cb319c9a26d
Luchao Qi <lc.qi@outlook.com>
2019-05-16 16:16:32 -0700

    lc

--- rna-seq/project_Homo_sapiens/4_counts/plot_cor_heatmap.R
+++ rna-seq/project_Homo_sapiens/4_counts/plot_cor_heatmap.R
@@ -4,8 +4,9 @@ options(stringsAsFactors = F)
 a = read.table('all.id.txt',header = T)
 meta = a[,1:6]
 exprSet = a[,7:ncol(a)]
+a1 = exprSet[,'SRR1039516']
 colnames(exprSet)
-
+group_list = colData(airway)[,3]
 
 
 library(corrplot)
@@ -22,4 +23,10 @@ pheatmap(scale(m))
 dev.off()
 
 
+
+a1 = data.frame(id = names(a1),a1=as.numeric(a1))
+a2 = data.frame(id = meta[,1],a2 = a2)
+a2$id = strsplit(a2$id,'\\.',simplify=T)[,1]
+temp = merge(a1,a2,by = 'id')
+plot(temp[,2:3])
 
