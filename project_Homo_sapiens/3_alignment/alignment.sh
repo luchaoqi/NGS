af35669d5f1196d7ae4a93c1b5ec26d6771ab87a
Luchao Qi <lc.qi@outlook.com>
2019-05-15 18:37:48 -0700

    lc

--- project_Homo_sapiens/3_alignment/alignment.sh
+++ project_Homo_sapiens/3_alignment/alignment.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # hisat2,subjunc,star,bwa,bowtie2 can be used to do alignment
-
 # note need to create index at first
+
+
 # subsample: cat for fq, zcat for gz
 # if no need to keep .gz as file extension use basename $id ".gz"
 ls /home/leon/rna-seq/project_Homo_sapiens/2_quality_control/trim_galore_output/*.fq| while read id; do ( cat $id|head -1000 > $(basename $id ));done
@@ -10,8 +11,7 @@ ls /home/leon/rna-seq/project_Homo_sapiens/2_quality_control/trim_galore_output/
 # bowtie2
 
 bowtie2-build chr22.fa chr22
-bowtie2 -x chr22 -1 sample/pair.1.fq -2 sample/pair.2.fq -S sample.sam
-
+bowtie2 -p 10 -x chr22 -1 sample/pair.1.fq -2 sample/pair.2.fq -S sample.sam
 
 # hisat2
 hisat2-build ref.fa ref
@@ -19,9 +19,29 @@ hisat2 -p 10 -x ref -1 $fq1 -2 $fq2 -S temp.sam
 
 # subjunc
 # build index at first!
-subjunc -T 5 -i $index -r $fq1 -R $fq2 -o temp.sam
+subjunc -T 5 -i $index -r $fq1 -R $fq2 -o temp.bam
 
 # bwa
 bwa mem -t 5 -M $index $fq1 $fq2 > temp.sam
 
+
+# batch processing after trimming QC
+ls *gz|cut -d"_" -f 1 |sort -u |while read id;do
+ls -lh ${id}_1_val_1.fq.gz   ${id}_2_val_2.fq.gz 
+hisat2 -p 10 -x /public/reference/index/hisat/hg38/genome -1 ${id}_1_val_1.fq.gz   -2 ${id}_2_val_2.fq.gz  -S ${id}.hisat.sam
+subjunc -T 5  -i /public/reference/index/subread/hg38 -r ${id}_1_val_1.fq.gz -R ${id}_2_val_2.fq.gz -o ${id}.subjunc.bam
+bowtie2 -p 10 -x /public/reference/index/bowtie/hg38  -1 ${id}_1_val_1.fq.gz   -2 ${id}_2_val_2.fq.gz  -S ${id}.bowtie.sam
+bwa mem -t 5 -M  /public/reference/index/bwa/hg38   ${id}_1_val_1.fq.gz   ${id}_2_val_2.fq.gz > ${id}.bwa.sam
+done
+
+
+# sam2bam
+ls *.sam|while read id; do ( samtools sort -O bam -@ 5 -o $(basename $id ".sam").bam $id );done
+
+# index for bam
+ls *.bam|xargs -i samtools index {}
+
+samtools view temp.bam | less -S
+samtools tview temp.bam
+samtools flagstat temp.bam
 
