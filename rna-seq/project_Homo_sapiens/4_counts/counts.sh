d55750c4db6ccaa5871356204502c7a2a3e23790
Luchao Qi <lc.qi@outlook.com>
2019-05-16 11:28:49 -0700

    lc

--- rna-seq/project_Homo_sapiens/4_counts/counts.sh
+++ rna-seq/project_Homo_sapiens/4_counts/counts.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+featureCounts -a $gtf -o $file $bam 
 
-featureCounts -a $gtf -o $file $bam
+# 如果一个个样本单独计数，输出多个文件使用代码是：
+for fn in {508..523}
+do
+featureCounts -T 5 -p -t exon -g gene_id  -a /public/reference/gtf/gencode/gencode.v25.annotation.gtf.gz -o $fn.counts.txt SRR1039$fn.bam
+done
+
+# 如果是批量样本的bam进行计数，使用代码是：
+mkdir $wkd/align 
+cd $wkd/align 
+source activate rna
 
 gtf="/public/reference/gtf/gencode/gencode.v25.annotation.gtf.gz"   
 featureCounts -T 5 -p -t exon -g gene_id  -a $gtf -o  all.id.txt  *.bam  1>counts.id.log 2>&1 &
