3e268e668c6e3d39cdb4cc3f20f2ea7aec3c3451
Luchao Qi <lc.qi@outlook.com>
2019-05-28 11:11:47 -0700

    lc

---
+++ GEO/0_install_packages.R
@@ -0,0 +1,27 @@
+rm(list = ls())   
+
+
+# https://bioconductor.org/packages/release/bioc/html/GEOquery.html
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("KEGG.db",ask = F,update = F)
+BiocManager::install(c("GSEABase","GSVA","clusterProfiler" ),ask = F,update = F)
+BiocManager::install(c("GEOquery","limma","impute" ),ask = F,update = F)
+BiocManager::install(c("genefu","org.Hs.eg.db","hgu133plus2.db" ),ask = F,update = F)
+
+install.packages('WGCNA')
+install.packages(c("FactoMineR", "factoextra"))
+install.packages(c("ggplot2", "pheatmap","ggpubr"))
+library("FactoMineR")
+library("factoextra")
+
+library(GSEABase)
+library(GSVA)
+library(clusterProfiler)
+library(genefu)
+library(ggplot2)
+library(ggpubr)
+library(hgu133plus2.db)
+library(limma)
+library(org.Hs.eg.db)
+library(pheatmap)
